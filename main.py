5d8f564c26ae3285ed536b34cf6efb7417b337ef
adityadkk06-commits <adityadkk06@gmail.com>
2026-05-13 05:32:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -216,7 +216,14 @@ async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 f"Vol Ratio: {r['VolRatio']}x\n\n"
             )
 
-        await update.message.reply_text(msg)
+        chart = generate_chart(r['Ticker'] + ".JK")
+
+        with open(chart, "rb") as photo:
+
+    await update.message.reply_photo(
+        photo=photo,
+        caption=msg
+    )
     # =========================
     # TOP SIGNALS
     # =========================
